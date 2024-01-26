67a1f118249e5f16b9f393db911b85a389e67cc9
Logan Dhillon <logandhi64@gmail.com>
2024-01-26 10:28:13 -0500

    Implement word list

--- main.py
+++ main.py
@@ -2,24 +2,27 @@
 
 import tkinter as tk
 from tkextrafont import Font
+import words, sys
 
 def check_answer():
-    if prompt == text_field.get():
-        prompt_label.config(text="WOOHOO")
+    if prompt.cget("text") == text_field.get():
+        prompt.config(text=word_list.pick(3))
 
-prompt = "the quick brown fox jumps over the lazy dog"
+word_list = words.WordList(words.DEFAULT)
 
 window = tk.Tk()
 window.title("SGA Trainer")
 
 # init SGA font
 sga_font = Font(file="resources/sga.ttf", family="Enchantment Proper")
+if sys.argv[1] == "--debug":
+    sga_font = ("", 16)
 
-prompt_label = tk.Label(window, text=prompt, font=sga_font)
+prompt = tk.Label(window, text=word_list.pick(3), font=sga_font)
 text_field = tk.Entry(window, width=64)
 
 tk.Label(window, text="What does this say?", font=("", 16)).pack(pady=10, padx=10)
-prompt_label.pack(pady=10, padx=10)
+prompt.pack(pady=10, padx=10)
 text_field.pack(pady=10, padx=10)
 tk.Button(window, text="Submit", command=check_answer).pack(pady=10)
 
