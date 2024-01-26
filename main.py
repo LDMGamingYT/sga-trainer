9e4b0608944c11e391fa5c06e3e4d5ccad3f8640
Logan Dhillon <logandhi64@gmail.com>
2024-01-26 10:30:52 -0500

    Add score and give up button

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ import words, sys
 
 def check_answer():
     if prompt.cget("text") == text_field.get():
-        prompt.config(text=word_list.pick(3))
+        regen_prompt()
+        score.config(text=score.cget("text") + 1)
+        
+def regen_prompt():
+    prompt.config(text=word_list.pick(3))
 
 word_list = words.WordList(words.DEFAULT)
 
@@ -18,12 +22,15 @@ sga_font = Font(file="resources/sga.ttf", family="Enchantment Proper")
 if sys.argv[1] == "--debug":
     sga_font = ("", 16)
 
+score = tk.Label(window, text=0)
 prompt = tk.Label(window, text=word_list.pick(3), font=sga_font)
 text_field = tk.Entry(window, width=64)
 
+score.pack()
 tk.Label(window, text="What does this say?", font=("", 16)).pack(pady=10, padx=10)
 prompt.pack(pady=10, padx=10)
 text_field.pack(pady=10, padx=10)
 tk.Button(window, text="Submit", command=check_answer).pack(pady=10)
+tk.Button(window, text="Give Up", command=regen_prompt).pack(pady=10)
 
 window.mainloop()
