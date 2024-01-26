c97b5d06b9b87de698f88aab55112140b7bd428e
Logan Dhillon <logandhi64@gmail.com>
2024-01-26 10:22:23 -0500

    Stop making useless variables

--- main.py
+++ main.py
@@ -15,14 +15,12 @@ window.title("SGA Trainer")
 # init SGA font
 sga_font = Font(file="resources/sga.ttf", family="Enchantment Proper")
 
-label = tk.Label(window, text="What does this say?", font=("", 16))
 prompt_label = tk.Label(window, text=prompt, font=sga_font)
 text_field = tk.Entry(window, width=64)
-button = tk.Button(window, text="Submit", command=check_answer)
 
-label.pack(pady=10, padx=10)
+tk.Label(window, text="What does this say?", font=("", 16)).pack(pady=10, padx=10)
 prompt_label.pack(pady=10, padx=10)
 text_field.pack(pady=10, padx=10)
-button.pack(pady=10)
+tk.Button(window, text="Submit", command=check_answer).pack(pady=10)
 
 window.mainloop()
