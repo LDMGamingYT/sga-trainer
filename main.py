b7575a20b49feeb720f877374356542f237b6acb
Logan Dhillon <logandhi64@gmail.com>
2024-01-26 02:41:55 -0500

    Working prototype complete

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ window.title("SGA Trainer")
 
 pyglet.font.add_file('sga.ttf')
 
-def on_button_click():
-    label.config(text="Hello, " + text_field.get())
+def check_answer():
+    print(prompt == text_field.get())
+    if prompt == text_field.get():
+        prompt_label.config(text="WOOHOO")
+
+prompt = "the quick brown fox jumps over the lazy dog"
 
 label = tk.Label(window, text="What does this say?", font=("", 16))
-prompt = tk.Label(window, text="the quick brown fox jumps over the lazy dog", font=("Enchantment Proper", 16))
-text_field = tk.Entry(window)
+prompt_label = tk.Label(window, text=prompt, font=("Enchantment Proper", 16))
+text_field = tk.Entry(window, width=64)
+button = tk.Button(window, text="Submit", command=check_answer)
 
 label.pack(pady=10, padx=10)
-prompt.pack(pady=10, padx=10)
+prompt_label.pack(pady=10, padx=10)
 text_field.pack(pady=10, padx=10)
+button.pack(pady=10)
 
 window.mainloop()
